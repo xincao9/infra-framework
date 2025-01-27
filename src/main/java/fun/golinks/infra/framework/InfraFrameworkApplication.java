90256ec8db406e7fe3a3b0c5f32c6ba0e3a15e0a
xincao9 <xincao9@gmail.com>
2025-01-27 23:10:33 +0800

    init

---
+++ src/main/java/fun/golinks/infra/framework/InfraFrameworkApplication.java
@@ -0,0 +1,13 @@
+package fun.golinks.infra.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fun/golinks/infra/framework/InfraFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.golinks.infra.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
