d6c4c247697de121bef3d44acef53dbf6a4754d2
xincao9 <xincao9@gmail.com>
2025-01-28 17:16:10 +0800

    添加测试代码

--- sample/src/main/java/fun/golinks/sample/Application.java
+++ sample/src/main/java/fun/golinks/sample/Application.java
@@ -1,9 +1,12 @@
 package fun.golinks.sample;
 
+import fun.golinks.trace.TraceAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(TraceAutoConfiguration.class)
 public class Application {
 
     public static void main(String... args) {
--- sample/src/main/java/fun/golinks/sample/controller/GreeterController.java
+++ sample/src/main/java/fun/golinks/sample/controller/GreeterController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("greeter")
 public class GreeterController {
 
     @GetMapping
