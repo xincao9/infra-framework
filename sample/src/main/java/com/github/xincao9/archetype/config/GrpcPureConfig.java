c9715af4608bd1c82bad1eb7a2443595c25f2ebc
xincao9 <xincao9@gmail.com>
2025-01-30 23:56:56 +0800

    OK

--- sample/src/main/java/com/github/xincao9/archetype/config/GrpcPureConfig.java
+++ sample/src/main/java/com/github/xincao9/archetype/config/GrpcPureConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GrpcPureConfig {
 
-    private static final String GREETER_APP_URL = "nacos://application";
+    private static final String GREETER_APP_URL = "nacos://greeter";
 
     @Bean
     public GreeterRPCServiceGrpc.GreeterRPCServiceBlockingStub greeterRPCServiceBlockingStub(
