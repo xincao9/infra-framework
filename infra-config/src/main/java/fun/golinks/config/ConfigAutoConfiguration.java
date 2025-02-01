573a167b110086c711eb55a268593f3ea5df8164
xincao9 <xincao9@gmail.com>
2025-02-01 20:45:34 +0800

    去掉无用的import

--- infra-config/src/main/java/fun/golinks/config/ConfigAutoConfiguration.java
+++ infra-config/src/main/java/fun/golinks/config/ConfigAutoConfiguration.java
@@ -3,8 +3,6 @@ package fun.golinks.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
-
 /**
  * 配置中心自动配置类
  */
