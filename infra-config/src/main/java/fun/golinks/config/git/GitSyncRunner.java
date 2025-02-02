0c801b276ee21370fe55e18fb818ade002fda3a5
xincao9 <xincao9@gmail.com>
2025-02-02 16:34:42 +0800

    优化代码

--- infra-config/src/main/java/fun/golinks/config/git/GitSyncRunner.java
+++ infra-config/src/main/java/fun/golinks/config/git/GitSyncRunner.java
@@ -16,7 +16,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -31,8 +30,9 @@ public class GitSyncRunner implements Runnable {
     private static final int DELAY = 30;
     private Git git;
     private String uri;
-    private String dir;
     private String appName;
+    private String repo;
+    private String dir;
     private String remote;
     private String remoteBranchName;
 
@@ -50,22 +50,12 @@ public class GitSyncRunner implements Runnable {
     /**
      * 构造器
      *
-     * @throws GitAPIException
-     *             git api异常
-     * @throws IOException
-     *             io异常
+     * @throws GitAPIException git api异常
+     * @throws IOException     io异常
      */
     private GitSyncRunner() throws Throwable {
         Map<String, String> configEnv = FileUtils.readConfig();
         if (configEnv.isEmpty()) {
-            return;
-        }
-        boolean enabled = Boolean.parseBoolean(configEnv.get(ConfigConsts.INFRA_CONFIG_ENABLED));
-        if (!enabled) {
-            return;
-        }
-        String type = configEnv.get(ConfigConsts.INFRA_CONFIG_TYPE);
-        if (!Objects.equals(type, GitConsts.GIT) && StringUtils.isNotBlank(type)) {
             return;
         }
         this.uri = configEnv.get(GitConsts.INFRA_CONFIG_GIT_URI);
@@ -74,6 +64,8 @@ public class GitSyncRunner implements Runnable {
         if (StringUtils.isAnyBlank(this.uri, this.appName)) {
             return;
         }
+        this.repo = StringUtils.substringAfterLast(this.uri, "/");
+        this.repo = StringUtils.substringBefore(this.repo, ".git");
         this.dir = configEnv.getOrDefault(GitConsts.INFRA_CONFIG_GIT_DIR, Paths.get(home, ".config").toString());
         this.remote = configEnv.getOrDefault(GitConsts.INFRA_CONFIG_GIT_REMOTE, "origin");
         this.remoteBranchName = configEnv.getOrDefault(GitConsts.INFRA_CONFIG_GIT_REMOTE_BRANCH_NAME, "main");
@@ -88,56 +80,45 @@ public class GitSyncRunner implements Runnable {
     /**
      * 初始化方法
      *
-     * @throws GitAPIException
-     *             git api异常
-     * @throws IOException
-     *             io异常
+     * @throws GitAPIException git api异常
+     * @throws IOException     io异常
      */
     private void init() throws GitAPIException, IOException {
-        String repo = StringUtils.substringAfterLast(this.uri, "/");
-        repo = StringUtils.substringBefore(repo, ".git");
-        this.git = createGit(repo);
+        this.git = createGit();
         this.run();
         ScheduledExecutorService scheduledExecutorService = Executors
                 .newSingleThreadScheduledExecutor(r -> new Thread(r, "GitSyncRunner"));
         scheduledExecutorService.scheduleAtFixedRate(this, DELAY, DELAY, TimeUnit.SECONDS);
     }
 
-    private Git createGit(String repo) throws GitAPIException, IOException {
-        Path path = Paths.get(this.dir, this.appName, repo, ".git");
-        log.info("path = {}", path);
+    private Git createGit() throws GitAPIException, IOException {
+        Path path = Paths.get(this.dir, this.appName, this.repo, ".git");
         if (path.toFile().exists()) {
             Repository repository = new RepositoryBuilder().setGitDir(path.toFile()).readEnvironment().findGitDir()
                     .build();
             return new Git(repository);
         }
-        return cloneRepository(repo);
+        return cloneRepository();
     }
 
     /**
      * clone仓库
      *
-     * @param repo
-     *            远程仓库
-     * 
      * @return git对象
-     * 
-     * @throws GitAPIException
-     *             git api 异常
+     * @throws GitAPIException git api 异常
      */
-    private Git cloneRepository(String repo) throws GitAPIException {
-        Path path = Paths.get(this.dir, this.appName, repo);
+    private Git cloneRepository() throws GitAPIException {
+        Path path = Paths.get(this.dir, this.appName, this.repo);
         return Git.cloneRepository().setURI(this.uri).setDirectory(path.toFile()).setTimeout(TIMEOUT).call();
     }
 
     /**
      * 同步拉取
      *
-     * @throws GitAPIException
-     *             git api 异常
+     * @throws GitAPIException git api 异常
      */
     private PullResult pull() throws GitAPIException {
-        return git.pull().setRemote(remote).setRemoteBranchName(remoteBranchName).call();
+        return this.git.pull().setRemote(this.remote).setRemoteBranchName(this.remoteBranchName).call();
     }
 
     /**
@@ -148,8 +129,8 @@ public class GitSyncRunner implements Runnable {
         try {
             PullResult pullResult = pull();
             if (pullResult.getMergeResult().getMergeStatus() == MergeResult.MergeStatus.FAST_FORWARD
-                    && !callbacks.isEmpty()) {
-                callbacks.forEach(Runnable::run);
+                    && !this.callbacks.isEmpty()) {
+                this.callbacks.forEach(Runnable::run);
             }
         } catch (GitAPIException e) {
             log.warn("GitSyncRunner failure!", e);
