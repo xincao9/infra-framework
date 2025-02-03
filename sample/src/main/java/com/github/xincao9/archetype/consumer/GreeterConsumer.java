7618ae4a6be53bf435e96dcb9f78ca8b0dae8c91
xincao9 <xincao9@gmail.com>
2025-02-03 11:47:29 +0800

    项目目录优化

---
+++ sample/src/main/java/com/github/xincao9/archetype/consumer/GreeterConsumer.java
@@ -0,0 +1,15 @@
+package com.github.xincao9.archetype.consumer;
+
+import com.github.xincao9.infra.archetype.GreeterSayRequest;
+import com.github.xincao9.infra.archetype.GreeterSayResponse;
+import fun.golinks.grpc.pure.util.GrpcConsumer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreeterConsumer {
+
+    public final GrpcConsumer<GreeterSayRequest, GreeterSayResponse> sayConsumer = GrpcConsumer
+            .wrap(greeterSayRequest -> GreeterSayResponse.newBuilder()
+                    .setMessage(String.format("Hello %s", greeterSayRequest.getName())).build());
+
+}
--- sample/src/main/java/com/github/xincao9/archetype/rpc/GreeterRPCServiceImpl.java
+++ sample/src/main/java/com/github/xincao9/archetype/rpc/GreeterRPCServiceImpl.java
@@ -1,21 +1,22 @@
 package com.github.xincao9.archetype.rpc;
 
+import com.github.xincao9.archetype.consumer.GreeterConsumer;
 import com.github.xincao9.infra.archetype.GreeterRPCServiceGrpc;
 import com.github.xincao9.infra.archetype.GreeterSayRequest;
 import com.github.xincao9.infra.archetype.GreeterSayResponse;
-import fun.golinks.grpc.pure.util.GrpcConsumer;
 import io.grpc.stub.StreamObserver;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class GreeterRPCServiceImpl extends GreeterRPCServiceGrpc.GreeterRPCServiceImplBase {
 
-    private static final GrpcConsumer<GreeterSayRequest, GreeterSayResponse> grpcConsumer = GrpcConsumer
-            .wrap(greeterSayRequest -> GreeterSayResponse.newBuilder()
-                    .setMessage(String.format("Hello %s", greeterSayRequest.getName())).build());
+    @Resource
+    private GreeterConsumer greeterConsumer;
 
     @Override
     public void say(GreeterSayRequest request, StreamObserver<GreeterSayResponse> responseObserver) {
-        grpcConsumer.accept(request, responseObserver);
+        greeterConsumer.sayConsumer.accept(request, responseObserver);
     }
 }
