222965cb9c68be4ecf7adecfa44e37cd27e061de
xincao9 <xincao9@gmail.com>
2025-02-03 18:57:17 +0800

    调试成功

--- infra-core/src/main/java/fun/golinks/core/CoreAutoConfiguration.java
+++ infra-core/src/main/java/fun/golinks/core/CoreAutoConfiguration.java
@@ -1,9 +1,17 @@
 package fun.golinks.core;
 
 import fun.golinks.core.config.WebConfig;
+import fun.golinks.core.interceptor.WebHandlerMethodReturnValueHandler;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @ImportAutoConfiguration(CoreAutoConfiguration.WebConfigImporter.class)
@@ -11,6 +19,21 @@ public class CoreAutoConfiguration {
 
     @ConditionalOnWebApplication
     @ImportAutoConfiguration(WebConfig.class)
-    public static class WebConfigImporter {
+    public static class WebConfigImporter implements InitializingBean {
+
+        @Resource
+        private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+        @Override
+        public void afterPropertiesSet() throws Exception {
+            // 搞不清楚 WebMvcConfigurer的addReturnValueHandlers方法添加，为什么不生效
+            List<HandlerMethodReturnValueHandler> handlers = new ArrayList<>();
+            handlers.add(new WebHandlerMethodReturnValueHandler());
+            List<HandlerMethodReturnValueHandler> oHandlers = requestMappingHandlerAdapter.getReturnValueHandlers();
+            if (oHandlers != null) {
+                handlers.addAll(oHandlers);
+            }
+            requestMappingHandlerAdapter.setReturnValueHandlers(handlers);
+        }
     }
 }
