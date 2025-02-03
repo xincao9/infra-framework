05514431f07432eeac823adcc4ae4b1240ce57dd
xincao9 <xincao9@gmail.com>
2025-02-03 18:28:16 +0800

    改为泗洪HandlerMethodReturnValueHandler

--- infra-core/src/main/java/fun/golinks/core/config/WebConfig.java
+++ infra-core/src/main/java/fun/golinks/core/config/WebConfig.java
@@ -1,9 +1,13 @@
 package fun.golinks.core.config;
 
 import fun.golinks.core.interceptor.WebHandlerInterceptor;
+import fun.golinks.core.interceptor.WebHandlerMethodReturnValueHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,5 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new WebHandlerInterceptor());
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> handlers) {
+        handlers.add(new WebHandlerMethodReturnValueHandler());
     }
 }
---
+++ infra-core/src/main/java/fun/golinks/core/interceptor/WebHandlerMethodReturnValueHandler.java
@@ -0,0 +1,29 @@
+package fun.golinks.core.interceptor;
+
+import fun.golinks.core.model.R;
+import fun.golinks.core.util.JsonUtils;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class WebHandlerMethodReturnValueHandler implements HandlerMethodReturnValueHandler {
+
+    @Override
+    public boolean supportsReturnType(MethodParameter returnType) {
+        return true;
+    }
+
+    @Override
+    public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest) throws Exception {
+        if (returnValue instanceof R) {
+            return;
+        }
+        mavContainer.setRequestHandled(true);
+        webRequest.getNativeResponse(HttpServletResponse.class).getWriter()
+                .write(JsonUtils.toJsonString(R.ok(returnValue)));
+    }
+}
