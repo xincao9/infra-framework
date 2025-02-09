40c46dcd63dbdee36789995c4245b3c3600b40df
xincao9 <xincao9@gmail.com>
2025-02-09 19:04:10 +0800

    优化openapi

--- sample/src/main/java/com/github/xincao9/archetype/config/OpenApiConfig.java
+++ sample/src/main/java/com/github/xincao9/archetype/config/OpenApiConfig.java
@@ -1,17 +1,34 @@
 package com.github.xincao9.archetype.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "infra-framework project", version = "v1"), servers = @Server(url = "/", description = "Default Server URL"), security = @SecurityRequirement(name = "Bearer Authentication"))
+@SecurityScheme(name = "Bearer Authentication", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI(@Value("${spring.application.name}") String applicationName) {
-        return new OpenAPI()
-                .info(new Info().title(applicationName).version("1.0.0").description("infra-framework project"));
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().components(new Components().addParameters("globalTraceHeader", new Parameter().in("header")
+                .name("trace-id").required(false).description("Global TraceId Header").schema(new StringSchema())));
+    }
+
+    @Bean
+    public OpenApiCustomiser customOpenApiCustomiser() {
+        return openApi -> openApi.getPaths().values()
+                .forEach(pathItem -> pathItem.readOperations().forEach(operation -> operation
+                        .addParametersItem(new Parameter().$ref("#/components/parameters/globalTraceHeader"))));
     }
 }
