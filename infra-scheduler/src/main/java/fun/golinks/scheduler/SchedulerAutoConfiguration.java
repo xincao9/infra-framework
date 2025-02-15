ebe76baea064be24356956c2fcbe216715c231e9
xincao9 <xincao9@gmail.com>
2025-02-15 13:29:03 +0800

    OK

--- infra-scheduler/src/main/java/fun/golinks/scheduler/SchedulerAutoConfiguration.java
+++ infra-scheduler/src/main/java/fun/golinks/scheduler/SchedulerAutoConfiguration.java
@@ -3,10 +3,8 @@ package fun.golinks.scheduler;
 import org.quartz.Scheduler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @ConditionalOnBean(Scheduler.class)
-@Configuration
 public class SchedulerAutoConfiguration {
 
     @Bean
