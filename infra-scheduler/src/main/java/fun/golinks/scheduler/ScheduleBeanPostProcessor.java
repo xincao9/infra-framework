adefd5200f6bca11c4c9e1c4d3d1d3cf39a90b34
xincao9 <xincao9@gmail.com>
2025-02-16 14:18:59 +0800

    OK

--- infra-scheduler/src/main/java/fun/golinks/scheduler/ScheduleBeanPostProcessor.java
+++ infra-scheduler/src/main/java/fun/golinks/scheduler/ScheduleBeanPostProcessor.java
@@ -5,15 +5,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.quartz.*;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 import org.springframework.util.ClassUtils;
 
 @Slf4j
-public class ScheduleBeanPostProcessor implements BeanPostProcessor, EnvironmentAware {
+public class ScheduleBeanPostProcessor implements BeanPostProcessor, EnvironmentAware, ApplicationContextAware {
 
     private final Scheduler scheduler;
     private Environment environment;
+    private ApplicationContext applicationContext;
 
     public ScheduleBeanPostProcessor(Scheduler scheduler) {
         this.scheduler = scheduler;
@@ -51,6 +54,11 @@ public class ScheduleBeanPostProcessor implements BeanPostProcessor, Environment
     @Override
     public void setEnvironment(Environment environment) {
         this.environment = environment;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
     public static class SchedulerBeansException extends BeansException {
