cc85baa8c68e868170e21388a19336a0b575790c
xincao9 <xincao9@gmail.com>
2025-02-19 13:12:57 +0800

    配置修改

--- sample/sample-biz/src/main/java/com/github/xincao9/archetype/config/GrpcPureConfig.java
+++ sample/sample-biz/src/main/java/com/github/xincao9/archetype/config/GrpcPureConfig.java
@@ -3,6 +3,7 @@ package com.github.xincao9.archetype.config;
 import com.github.xincao9.infra.archetype.GreeterRPCServiceGrpc;
 import fun.golinks.grpc.pure.GrpcChannels;
 import io.grpc.ManagedChannel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,12 +16,14 @@ public class GrpcPureConfig {
     /**
      * nacos注册中心请使用 nacos://{应用名} 格式定义target
      */
-    private static final String GREETER_APP_URL = "nacos://greeter";
+    private static final String GREETER_APP_URL = "nacos://";
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     @Bean
     public GreeterRPCServiceGrpc.GreeterRPCServiceBlockingStub greeterRPCServiceBlockingStub(
             GrpcChannels grpcChannels) {
-        ManagedChannel channel = grpcChannels.create(GREETER_APP_URL);
+        ManagedChannel channel = grpcChannels.create(GREETER_APP_URL + applicationName);
         return GreeterRPCServiceGrpc.newBlockingStub(channel);
     }
 }
