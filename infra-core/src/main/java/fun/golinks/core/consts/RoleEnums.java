b3e61a42fbb987bf129f746d00888ac0c7e64f84
xincao9 <xincao9@gmail.com>
2025-02-23 13:59:34 +0800

    优化

--- infra-core/src/main/java/fun/golinks/core/consts/RoleEnums.java
+++ infra-core/src/main/java/fun/golinks/core/consts/RoleEnums.java
@@ -7,8 +7,10 @@ import java.util.Objects;
 
 @Getter
 public enum RoleEnums {
-    TOURIST(1, "tourist", "游客"), NORMAL((1 << 1) + 1, "normal", "普通用户"), VIP((1 << 2) + (1 << 1) + 1, "vip", "VIP用户"),
-    ADMIN((1 << 3) + (1 << 2) + (1 << 1) + 1, "admin", "管理员");
+    TOURIST(1, "tourist", "游客"),
+    NORMAL((1 << 1) + TOURIST.no, "normal", "普通用户"),
+    VIP((1 << 2) + NORMAL.no, "vip", "VIP用户"),
+    ADMIN((1 << 3) + VIP.no, "admin", "管理员");
 
     /**
      * 编号
@@ -48,11 +50,8 @@ public enum RoleEnums {
      * <p>
      * 该函数通过比较两个角色枚举的编号（no）的位运算结果，来判断a是否包含b的所有权限。 具体来说，如果a的编号与b的编号进行按位与运算的结果等于b的编号，则说明a包含b的所有权限。
      *
-     * @param a
-     *            第一个角色枚举，表示待检查的角色
-     * @param b
-     *            第二个角色枚举，表示需要包含的角色
-     * 
+     * @param a 第一个角色枚举，表示待检查的角色
+     * @param b 第二个角色枚举，表示需要包含的角色
      * @return 如果a包含b的所有权限，则返回true；否则返回false
      */
     public static Boolean contain(RoleEnums a, RoleEnums b) {
