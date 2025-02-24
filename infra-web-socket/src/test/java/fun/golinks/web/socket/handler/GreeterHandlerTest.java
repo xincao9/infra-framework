928cc80f22aba5e6625c0df0b9cc527b95df1279
xincao9 <xincao9@gmail.com>
2025-02-24 22:16:52 +0800

    添加测试用例

--- infra-web-socket/src/test/java/fun/golinks/web/socket/handler/GreeterHandlerTest.java
+++ infra-web-socket/src/test/java/fun/golinks/web/socket/handler/GreeterHandlerTest.java
@@ -1,19 +1,87 @@
 package fun.golinks.web.socket.handler;
 
+import fun.golinks.web.socket.GreeterRequest;
+import fun.golinks.web.socket.MessageNoEnums;
+import fun.golinks.web.socket.WebSocketMessage;
 import fun.golinks.web.socket.config.SystemConfig;
+import fun.golinks.web.socket.core.WebSocketFrameHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
+import io.netty.handler.codec.http.websocketx.WebSocketVersion;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
 @SpringBootTest(classes = SystemConfig.class)
 public class GreeterHandlerTest {
 
     @BeforeEach
     public void setup() throws Exception {
-
     }
 
     @Test
-    public void testHandle() {
+    public void testHandle() throws Throwable {
+        EventLoopGroup group = new NioEventLoopGroup();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            URI uri = new URI("ws://localhost:8888/ws");
+            WebSocketClientHandshaker webSocketClientHandshaker = WebSocketClientHandshakerFactory.newHandshaker(
+                    uri, WebSocketVersion.V13, null, true, new DefaultHttpHeaders());
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new HttpServerCodec());
+                            pipeline.addLast(new HttpObjectAggregator(65536));
+                            pipeline.addLast(new WebSocketFrameHandler());
+                            pipeline.addLast(new ProtobufVarint32FrameDecoder());
+                            pipeline.addLast(new ProtobufDecoder(WebSocketMessage.getDefaultInstance()));
+                            pipeline.addLast(new ProtobufVarint32LengthFieldPrepender());
+                            pipeline.addLast(new ProtobufEncoder());
+                            pipeline.addLast(new SimpleChannelInboundHandler<WebSocketMessage>() {
+
+                                @Override
+                                protected void channelRead0(ChannelHandlerContext ctx, WebSocketMessage msg) throws Exception {
+                                    log.info("msg{}", msg);
+                                    countDownLatch.countDown();
+                                }
+                            });
+                        }
+                    });
+            Channel channel = bootstrap.connect("localhost", 8888).sync().channel();
+            webSocketClientHandshaker.handshake(channel);
+            GreeterRequest greeterRequest = GreeterRequest.newBuilder()
+                    .setName("xincao")
+                    .build();
+            WebSocketMessage webSocketMessage = WebSocketMessage.newBuilder()
+                    .setNo(MessageNoEnums.GREETER_REQUEST_VALUE)
+                    .setPayload(greeterRequest.toByteString())
+                    .build();
+            channel.writeAndFlush(new BinaryWebSocketFrame(Unpooled.wrappedBuffer(webSocketMessage.toByteArray())));
+        } finally {
+            group.shutdownGracefully();
+        }
+        countDownLatch.await();
     }
 }
